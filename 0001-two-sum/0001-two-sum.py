a5fc1b3d6d4dc6b9a1b7ccb82f16717d1159d08d
skandhapavithras3010-ux <skandhapavithras3010@gmail.com>
2026-07-09 01:20:18 +0530

    Time: 3 ms (53.82%), Space: 20.5 MB (40.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self,nums,target):
+        hash = {}
+
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-
-
-                    # 2 SUM BRUTE FORCE
-                    
+            need = target - nums[i]
+            if need in hash:
+                return [hash[need],i]
+            hash[nums[i]] = i
+            
