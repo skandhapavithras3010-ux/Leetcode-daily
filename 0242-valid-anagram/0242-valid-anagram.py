074c1a684bf61a1b111809ee38f886ff9efc6d76
skandhapavithras3010-ux <skandhapavithras3010@gmail.com>
2026-07-11 20:16:00 +0530

    Time: 15 ms (44.37%), Space: 20.3 MB (17.91%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s1 = sorted(s)
+        t1 = sorted(t)
+        if s1 == t1:
+            return True
+        return False
+
+        # IN BUILT PYTHON FUNCTION USAGE
+        
