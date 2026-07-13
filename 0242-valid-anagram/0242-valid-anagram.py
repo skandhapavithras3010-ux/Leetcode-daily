3012a9dbeb8242f9896580d187784477b21ca77e
skandhapavithras3010-ux <skandhapavithras3010@gmail.com>
2026-07-13 17:13:21 +0530

    Time: 12 ms (55.79%), Space: 19.4 MB (45.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,10 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s1 = sorted(s)
-        t1 = sorted(t)
-        if s1 == t1:
-            return True
-        return False
-
-        # IN BUILT PYTHON FUNCTION USAGE
-        
+        if len(s) != len(t):
+            return False
+        count = {}
+        # Counting the characters in s
+        for c in s:
+            if c in count:
+                count[c] += 1
+            else:
+                count[c] = 1
+        # Removing the characters using t
+        for c in t:
+            if c not in count:
+                return False
+            count[c] -= 1
+            if count[c] < 0:
+                return False
+        return True
